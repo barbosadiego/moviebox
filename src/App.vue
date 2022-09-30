979811cdaaa5c2a25d1c9a25733277e53e0c8a46
diegobarbosa <diego.gutemberg@hotmail.com>
2022-09-30 11:28:39 -0300

    header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
   <div id="app">
-    
+    <Header />
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header.vue';
+
+export default {
+  components:{
+    Header,
+  }
+}
+</script>
+
 <style lang="scss">
+:root{
+  --text-color: #fff;
+  --border-color: #D1D5DB;
+  --rose: #E11D48;
+  --white: #fff;
+}
+
+*{
+  box-sizing: border-box;
+}
+
+img{
+  max-width: 100%;
+  display: block;
+}
+
+body, h1, h2, h3, p, ul, li {
+  margin: 0px;
+  padding: 0px;
+  font-family: 'DM Sans';
+}
+
+.container{
+  max-width: 1240px;
+  margin: 0 auto;
+}
+
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,113 @@
 <template>
-  <header>
-    
+  <header class="header">
+    <nav class="header__nav container">
+      <div class="logo">
+        <img src="@/assets/Logo.svg" alt="" />
+      </div>
+      <div class="search">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="What do you want do watch?"
+        />
+        <img src="@/assets/Search.svg" alt="" />
+      </div>
+      <div class="signin">
+        <span>sing in</span>
+        <div class="menu-area">
+          <div class="menu"></div>
+        </div>
+      </div>
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
-
-}
+  name: 'HeaderComponent',
+};
 </script>
 
 <style lang="scss" scoped>
+@import '@/Functions';
 
+.header {
+  background-color: #000;
+
+  &__nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 50px;
+    color: var(--text-color);
+    padding: 15px 95px;
+
+    .logo {
+      height: 50px;
+    }
+
+    .search {
+      flex: 1 0 270px;
+      max-width: 525px;
+      position: relative;
+      display: flex;
+      justify-content: space-between;
+      border: 2px solid var(--border-color);
+      padding: 6px 10px;
+      border-radius: 6px;
+
+      input {
+        width: 100%;
+        background-color: transparent;
+        border: none;
+        padding: 6px 10px;
+        outline: none;
+        color: var(--text-color);
+
+        &::placeholder {
+          font-family: inherit;
+          color: var(--text-color);
+          font-size: rem(16);
+          line-height: rem(24);
+        }
+      }
+    }
+
+    .signin {
+      display: flex;
+      align-items: center;
+      gap: 27px;
+
+      span {
+        display: inline-block;
+        font-size: rem(16);
+        font-weight: 700;
+        line-height: rem(24);
+        text-transform: capitalize;
+      }
+
+      .menu-area{
+        width: 36px;
+        height: 36px;
+        background-color: var(--rose);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        cursor: pointer;
+        
+        .menu {
+          display: inline-block;
+          width: 20px;
+          height: 3px;
+          background-color: var(--white);
+          border-radius: 2px;
+          box-shadow: 0px 10px ;
+          transform: translateY(-5px);
+        }
+      }
+    }
+  }
+}
 </style>
